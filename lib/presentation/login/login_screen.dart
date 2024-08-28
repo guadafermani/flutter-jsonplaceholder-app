88e4b9bcc076cdaaaaf00e170e27f9a48cf78d81
Guadalupe Fermani <guadalupefermani@gmail.com>
2024-08-28 02:28:14 -0300

    add login screen

---
+++ lib/presentation/login/login_screen.dart
@@ -0,0 +1,49 @@
+// lib/presentation/login/login_screen.dart
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_emailController.text == 'challenge@fudo' &&
+                    _passwordController.text == 'password') {
+                  // TODO: Navigate to posts screen
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Invalid credentials')),
+                  );
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
